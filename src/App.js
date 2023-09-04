5bcb049cbb2f90566447c846ba405a05b50f9637
gitdjay <tiwaridhananjay70@gmail.com>
2023-09-05 00:27:20 +0530

    fixed warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import WatchedSummary from "./components/WatchedSummary";
 import Navbar from "./components/Navbar";
